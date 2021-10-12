f2a1394ffbfb7d05742d60fcd313d7716965f7e5
Khan Fayjul <khanuxd@gmail.com>
2021-10-12 20:34:31 +0300

    compoents added for Home page structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Layout from 'antd/lib/layout';
+import HeroBanner from '../component/banner/HeroBanner';
+import Campaign from '../component/Campaign';
+import Join from '../assets/images/join.png';
+import HowItWorks from '../component/HowItWorks';
+import GlobalActivities from '../component/GlobalActivities';
+import CTABanner from '../component/banner/CTABanner';
 
 const { Content } = Layout;
 
 const Home = () => {
   return (
-    <Content>
-      <h3>Home Page</h3>
+    <Content className="main-area">
+      <HeroBanner />
+      <Campaign />
+      <HowItWorks />
+      <GlobalActivities />
+      <CTABanner heading="Be A Part Of Our Proud Community" bodyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et." btnVal="Join Now" img={Join} />
     </Content>
   );
 };
