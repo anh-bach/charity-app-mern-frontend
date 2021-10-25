cedc58e738460f5eb11792459ccb5a59670e60ea
Khan Fayjul <khanuxd@gmail.com>
2021-10-25 13:36:15 +0300

    reset password form className issue fixed

--- src/pages/auth/ResetPassword.js
+++ src/pages/auth/ResetPassword.js
@@ -5,26 +5,26 @@ import ResetPasswordForm from '../../component/form/ResetPasswordForm';
 
 const ForgotPassword = () => {
   return (
-    <section className="register">
-      <Row className="register_area">
-        <Col className="ant-col-xs-24 ant-col-md-24 ant-col-lg-12 ant-col-xl-12 register__area__left">
-          <div className="register__area__left-area">
-            <img className="register__area__left-area--img" src={HoldLove} alt='starry night' width={'100%'} />
+    <section className="reset">
+      <Row className="reset_area">
+        <Col className="ant-col-xs-24 ant-col-md-24 ant-col-lg-12 ant-col-xl-12 reset__area__left">
+          <div className="reset__area__left-area">
+            <img className="reset__area__left-area--img" src={HoldLove} alt='starry night' width={'100%'} />
           </div>
         </Col>
-        <Col className="ant-col-xs-24 ant-col-md-24 ant-col-lg-12 ant-col-xl-12 register__area__right">
-          <div className="register__area__right-block">
-            <div className="register__area__right-block-top">
-              <h2 className="register__area__right-block-top--logo heading--2">MyHappyFund</h2>
+        <Col className="ant-col-xs-24 ant-col-md-24 ant-col-lg-12 ant-col-xl-12 reset__area__right">
+          <div className="reset__area__right-block">
+            <div className="reset__area__right-block-top">
+              <h2 className="reset__area__right-block-top--logo heading--2">MyHappyFund</h2>
               <div>
-                <h3 className="register__area__right-block-top--heading heading--3">Reset Password</h3>
-                <p className="register__area__right-block-top--sub-heading">
+                <h3 className="reset__area__right-block-top--heading heading--3">Reset Password</h3>
+                <p className="reset__area__right-block-top--sub-heading">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.Doloremque,
                   ratione.
                 </p>
               </div>
             </div>
-            <div className="register__area__right-block-middle">
+            <div className="reset__area__right-block-bottom">
               <ResetPasswordForm />
             </div>
           </div>
