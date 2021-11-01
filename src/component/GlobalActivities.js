52808b53046b081aa59965a378bd43a79c172869
Khan Fayjul <khanuxd@gmail.com>
2021-11-01 17:46:46 +0200

    responsiveness issue fixed

--- src/component/GlobalActivities.js
+++ src/component/GlobalActivities.js
@@ -15,13 +15,13 @@ const GlobalActivities = () => {
                     </Col>
                 </Row>
                 <Row gutter={[32, 32]} className="global-activities__area--bottom">
-                    <Col className="ant-col-xs-24 ant-col-md-12 ant-col-lg-12 ant-col-xl-8 global-activities__area--bottom--col">
+                    <Col className="ant-col-xs-24 ant-col-md-12 ant-col-lg-8 ant-col-xl-8 global-activities__area--bottom--col">
                         <FeatureCard keyVal="10K" heading="Worldwide Campaigns" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, " />
                     </Col>
-                    <Col className="ant-col-xs-24 ant-col-md-12 ant-col-lg-12 ant-col-xl-8 global-activities__area--bottom--col">
+                    <Col className="ant-col-xs-24 ant-col-md-12 ant-col-lg-8 ant-col-xl-8 global-activities__area--bottom--col">
                         <FeatureCard keyVal="$600M" heading="Fund Raised" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, " />
                     </Col>
-                    <Col className="ant-col-xs-24 ant-col-md-12 ant-col-lg-12 ant-col-xl-8 global-activities__area--bottom--col">
+                    <Col className="ant-col-xs-24 ant-col-md-12 ant-col-lg-8 ant-col-xl-8 global-activities__area--bottom--col">
                         <FeatureCard keyVal="200M" heading="Worldwide Doners" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit, hendrerit elit et, " />
                     </Col>
                 </Row>
